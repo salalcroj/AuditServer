fa53311ac23f453952888ea8598fdf7dc9be7ab4
salalcroj <salalcroj@alum.us.es>
2026-06-04 20:31:03 +0200

    Modificar endpoint /readyz para incluir remoteRepo y actualizar values.yaml

--- src/main/java/us/muit/fs/samples/auditserver/controllers/HealthController.java
+++ src/main/java/us/muit/fs/samples/auditserver/controllers/HealthController.java
@@ -1,8 +1,6 @@
 package us.muit.fs.samples.auditserver.controllers;
 
-
 import java.util.HashMap;
-
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -12,14 +10,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import us.muit.fs.a4i.model.remote.GitHubRepositoryEnquirer;
 import us.muit.fs.a4i.model.remote.RemoteEnquirer;
 import us.muit.fs.a4i.model.entities.Metric;
-
 import us.muit.fs.samples.auditserver.config.AppProperties;
-
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
@@ -31,46 +27,44 @@ public class HealthController {
 	{
 		return config.getHealthzGithubRepo();
 	}
-	/*
-	Kubernetes usa los recursos (o endpoints) /healthz/livez/readyz para validar el estado del servicio y sus dependencias.
-	Al realizar un GET a estos recursos kubernetes puede saber, en cualquier momento, si el servicio esta operativo.
-	En caso afirmativo, recibie 200 OK. En cualquier otro caso 500. Este endpoint lo consume directamente la
-	readinessProbe de Kubernetes para gestionar de manera automatica el service
-	discovery.
-	Consultar https://kubernetes.io/docs/reference/using-api/health-checks/
-*/
-@RequestMapping("/readyz")
-@GetMapping(path = "/readyz", produces=MediaType.APPLICATION_JSON_VALUE)
-ResponseEntity<Map<String, Object>> healthz() {
-	/*
-		En esta seccion se evaluan las dependencias del servicio
-		Por ejemplo verifica que los backend (en este caso la API de github, a traves de Audit4Improve)
-		Para ello se solicita la metrica totalAdditions del repositorio que este configurado en el fichero de configuracion
-	*/
-	try{
-		
-		String healthzGithubRepo = this.getHealthzGithubRepo();
-		Map<String, Object> body = new HashMap<>();
-		RemoteEnquirer remote = new GitHubRepositoryEnquirer();
-		
-		Metric myMetric = remote.getMetric("totalAdditions",healthzGithubRepo);
-	    
-		if(((Integer)myMetric.getValue()!=0) && (myMetric.getName()=="totalAdditions")){
-			body.put("healthy", true);
-			body.put("totalAdditions", myMetric.getValue());
-			body.put("metric", myMetric);
-			log.fine("La respuesta recibida ha sido: "+myMetric);
-		}else {
-			log.fine("La respuesta del remoto no se ha recibido bien");
+	
+	@RequestMapping("/readyz")
+	@GetMapping(path = "/readyz", produces=MediaType.APPLICATION_JSON_VALUE)
+	ResponseEntity<Map<String, Object>> healthz() {
+		try {
+			String healthzGithubRepo = this.getHealthzGithubRepo();
+			Map<String, Object> body = new HashMap<>();
+			RemoteEnquirer remote = new GitHubRepositoryEnquirer();
+			
+			Metric myMetric = remote.getMetric("totalAdditions", healthzGithubRepo);
+			
+			if (((Integer)myMetric.getValue() != 0) && (myMetric.getName().equals("totalAdditions"))) {
+				body.put("healthy", true);
+				body.put("totalAdditions", myMetric.getValue());
+				body.put("metric", myMetric);
+				body.put("remoteRepo", healthzGithubRepo);
+				log.fine("La respuesta recibida ha sido: " + myMetric);
+			} else {
+				log.fine("La respuesta del remoto no se ha recibido bien");
+				body.put("healthy", false);
+				body.put("remoteRepo", healthzGithubRepo);
+			}
+			return ResponseEntity.status(HttpStatus.OK).body(body);
+		} catch(Exception ref) {
+			Map<String, Object> body = new HashMap<>();
+			log.fine("Se ha recibido esta excepción: " + ref);
+			
+			body.put("healthy", false);
+			body.put("error", ref.getMessage());
+			
+			// FUERZA BRUTA PARA EL EJERCICIO: Introducir el repositorio aunque la red falle
+			try {
+				body.put("remoteRepo", this.getHealthzGithubRepo());
+			} catch(Exception e) {
+				body.put("remoteRepo", "unknown");
+			}
+			
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
 		}
-		return ResponseEntity.status(HttpStatus.OK).body(body);
-	}	catch(Exception ref) {
-		Map<String, Object> body = new HashMap<>();
-        log.fine("Se ha recibido esta excepción: "+ref);
-		body.put("healthy", false);
-		body.put("error", ref.getMessage());
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
 	}
-}
-
 }
